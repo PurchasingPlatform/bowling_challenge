b66720d0b60b75e0f90cf67aa64af3858daf9c72
Jeremy Herzberg <darmot7@gmail.com>
2015-07-22 19:02:27 -0500

    All tests green

--- score_keeperb.rb
+++ score_keeperb.rb
@@ -9,7 +9,7 @@ class ScoreKeeper
   end
 
   #finds the amount of frames taken up during a game of bowling
-  #a game of bowling can only take 20 or 21 frames to complete
+  #a game of bowling can take up to 20 or 21 frames to complete
   def compute_game_length(input)
     adjusted_game_length = 0
     (0..input.length-1).each do |char|
@@ -38,15 +38,14 @@ class ScoreKeeper
     score = 0
 
     (0..input.length-1).each do |throw|
-    input[throw].downcase #incase of an accidental X input
+     #input[throw].downcase #just in case of "X"
 
-    if number_of_throws < 18 #after the 18th throw use special 10th frame scoring
+    if number_of_throws < 18 #after the 16th throw use special 10th frame scoring
 
 
-      #strike
+
       if input[throw] == "x"
 
-        #if a strike then a spare
         if input[throw + 2] == "/"
           score += convert_to_number(input,throw) + convert_to_number(input,throw + 2)
           number_of_throws += 2
@@ -55,7 +54,7 @@ class ScoreKeeper
           score += convert_to_number(input,throw) + convert_to_number(input,throw + 1) + convert_to_number(input,throw + 2)
           number_of_throws += 2
         end
-      #spare
+
       elsif input[throw] == "/"
         score += convert_to_number(input,throw) + convert_to_number(input,throw + 1) - convert_to_number(input,throw - 1)
         number_of_throws += 1
@@ -65,18 +64,20 @@ class ScoreKeeper
 
       end
 
+      if number_of_throws.odd? && input[throw] != "x"
+        raise ArgumentError, 'Impossible roll Detected' if input[throw] == "/"
+        raise ArgumentError, 'Impossible roll Detected' if input[throw + 1] != "/" && convert_to_number(input,throw) + convert_to_number(input,throw + 1) > 9
+      end
 
-
-      #the 10th frame scoring rules
     else
-      #strike
+
       if input[throw] == "x"
         score += convert_to_number(input,throw)
         number_of_throws += 1
-      #spare
+
       elsif input[throw] == "/"
         score += convert_to_number(input,throw) - convert_to_number(input,throw - 1)
-
+        #score += convert_to_number(input,throw + 1) if input[throw + 1] != nil
         number_of_throws += 1
 
       else score += convert_to_number(input,throw)
@@ -84,13 +85,10 @@ class ScoreKeeper
 
       end
 
-
-    end
-
-    #check for an impossible roll combination like "999" or two spares "/" "/" in a row in the first and second rolls of a frame
-    if number_of_throws.odd? && input[throw] != "x"
-      raise ArgumentError, 'Impossible roll Detected' if input[throw] == "/"
-      raise ArgumentError, 'Impossible roll Detected' if input[throw + 1] != "/" && convert_to_number(input,throw) + convert_to_number(input,throw + 1) > 9 #only do the check if a spare isn't the second throw
+      if number_of_throws.odd? && input[throw] != "x"
+        raise ArgumentError, 'Impossible roll Detected' if input[throw] == "/"
+        raise ArgumentError, 'Impossible roll Detected' if input[throw + 1] != "/" && convert_to_number(input,throw) + convert_to_number(input,throw + 1) > 9
+      end
     end
 
     end
